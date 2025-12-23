db22ddb3bd493612b82bd152eca2132b02527416
Gabriel Gutierrez <gabe.gg@icloud.com>
2025-12-23 11:56:12 -0800

    Initial Commit

---
+++ ArcintelTests/ArcintelTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArcintelTests.swift
+//  ArcintelTests
+//
+//  Created by Claudia&Gabe  on 12/23/25.
+//
+
+import Testing
+@testable import Arcintel
+
+struct ArcintelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
